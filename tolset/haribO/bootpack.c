9c43c4bcb6c5e60a99c2e12216539bbd446448d9
sp4ghet <sp4ghet@users.noreply.github.com>
2018-10-08 14:47:31 +0100

    Render text

--- tolset/haribO/bootpack.c
+++ tolset/haribO/bootpack.c
@@ -3,6 +3,7 @@ void io_cli(void);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
+extern char hankaku[4096];
 
 #define COL8_000000 0
 #define COL8_ff0000 1
@@ -25,33 +26,28 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 
 void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(char* vram, int screenX, int screenY);
+void putfont8(char* vram, int xsize, int x, int y, char c, char *font);
+
+struct BOOTINFO {
+    char cylinders, leds, videoMode, reserve;
+    short screenX, screenY;
+    char* vram;
+};
 
 void HariMain(void){
     
     init_palette();
 
-    char* vram = (char*) 0xa0000;
+    struct BOOTINFO *binfo = 0x00ff0;
 
-    int xsize = 320;
-    int ysize = 200;
+    int xsize = binfo->screenX;
+    int ysize = binfo->screenY;
+    char* vram = binfo->vram;
     
-    boxfill(vram, xsize, COL8_008484, 0,          0, xsize - 1, ysize - 29);
-    boxfill(vram, xsize, COL8_c6c6c6, 0, ysize - 28, xsize - 1, ysize - 28);
-    boxfill(vram, xsize, COL8_ffffff, 0, ysize - 27, xsize - 1, ysize - 27);
-    boxfill(vram, xsize, COL8_c6c6c6, 0, ysize - 26, xsize - 1, ysize -  1);
+    init_screen(vram, xsize, ysize);
 
-    boxfill(vram, xsize, COL8_ffffff,  3, ysize - 24, 59, ysize - 24);
-    boxfill(vram, xsize, COL8_ffffff,  2, ysize - 24,  2, ysize - 4);
-    boxfill(vram, xsize, COL8_848484,  3, ysize -  4, 59, ysize - 4);
-    boxfill(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
-    boxfill(vram, xsize, COL8_000000,  2, ysize -  3, 59, ysize - 3);
-    boxfill(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
-
-
-    boxfill(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
-    boxfill(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
-    boxfill(vram, xsize, COL8_000000, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
-    boxfill(vram, xsize, COL8_000000, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+    putfont8(vram, xsize, 8, 8, COL8_ffffff, hankaku + 'A'*16);
 
     for(;;){
         io_hlt();
@@ -72,6 +68,28 @@ void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, in
         }
         vram += xsize;
     }
+}
+
+void init_screen(char* vram, int screenX, int screenY){
+    boxfill(vram, screenX, COL8_008484, 0,          0, screenX - 1, screenY - 29);
+    boxfill(vram, screenX, COL8_c6c6c6, 0, screenY - 28, screenX - 1, screenY - 28);
+    boxfill(vram, screenX, COL8_ffffff, 0, screenY - 27, screenX - 1, screenY - 27);
+    boxfill(vram, screenX, COL8_c6c6c6, 0, screenY - 26, screenX - 1, screenY -  1);
+
+    boxfill(vram, screenX, COL8_ffffff,  3, screenY - 24, 59, screenY - 24);
+    boxfill(vram, screenX, COL8_ffffff,  2, screenY - 24,  2, screenY - 4);
+    boxfill(vram, screenX, COL8_848484,  3, screenY -  4, 59, screenY - 4);
+    boxfill(vram, screenX, COL8_848484, 59, screenY - 23, 59, screenY - 5);
+    boxfill(vram, screenX, COL8_000000,  2, screenY -  3, 59, screenY - 3);
+    boxfill(vram, screenX, COL8_000000, 60, screenY - 24, 60, screenY - 3);
+
+
+    boxfill(vram, screenX, COL8_848484, screenX - 47, screenY - 24, screenX -  4, screenY - 24);
+    boxfill(vram, screenX, COL8_848484, screenX - 47, screenY - 23, screenX - 47, screenY -  4);
+    boxfill(vram, screenX, COL8_000000, screenX - 47, screenY -  3, screenX -  4, screenY -  3);
+    boxfill(vram, screenX, COL8_000000, screenX -  3, screenY - 24, screenX -  3, screenY -  3);
+
+    return;
 }
 
 void init_palette(void){
@@ -113,3 +131,23 @@ void set_palette(int start, int end, unsigned char *rgb){
     io_store_eflags(eflags);
     return;
 }
+
+void putfont8(char* vram, int xsize, int x, int y, char color, char *font){
+    char d;
+    int i;
+    for(i=0; i<16; i++){
+        d = font[i];
+        char* p = vram + (y+i)*xsize + x; //pointer for this row
+
+        if((d & 0x80) != 0){ p[0] = color; } //leftmost 0b10000000 -> 0x80
+        if((d & 0x40) != 0){ p[1] = color; }
+        if((d & 0x20) != 0){ p[2] = color; }
+        if((d & 0x10) != 0){ p[3] = color; }
+        if((d & 0x08) != 0){ p[4] = color; }
+        if((d & 0x04) != 0){ p[5] = color; }
+        if((d & 0x02) != 0){ p[6] = color; }
+        if((d & 0x01) != 0){ p[7] = color; } //rightmost 0b00000001 -> 0x01
+    }
+    return;
+}
+
