2a465f60693b9c51ff6ec0c6c41a573ce17b3b05
sp4ghet <sp4ghet@users.noreply.github.com>
2018-10-10 16:07:42 +0100

    End of day 5, GDT and IDT

--- tolset/haribO/bootpack.c
+++ tolset/haribO/bootpack.c
@@ -1,9 +1,18 @@
+#include <stdio.h>
+
+#pragma region assembly defined
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
-extern char hankaku[4096];
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
+#pragma endregion
+
+
+#pragma region macros
 
 #define COL8_000000 0
 #define COL8_ff0000 1
@@ -22,12 +31,9 @@ extern char hankaku[4096];
 #define COL8_008484 14
 #define COL8_848484 15
 
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
+#pragma endregion
 
-void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen(char* vram, int screenX, int screenY);
-void putfont8(char* vram, int xsize, int x, int y, char c, char *font);
+#pragma region structs
 
 struct BOOTINFO {
     char cylinders, leds, videoMode, reserve;
@@ -35,19 +41,67 @@ struct BOOTINFO {
     char* vram;
 };
 
+struct SEGMENT_DESCRIPTOR {
+    short limit_low, base_low;
+    char base_mid, access_right;
+    char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+    short offset_low, selector;
+    char dw_count, access_right;
+    short offset_high;
+};
+
+#pragma endregion
+
+#pragma region function declarations
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(char* vram, int screenX, int screenY);
+
+void putfont8(char* vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asci(char* vram, int xsize, int x, int y, char color, unsigned char *s);
+
+void init_cursor(char *mouse, char color);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+
+#pragma endregion
+
+
 void HariMain(void){
     
     init_palette();
 
-    struct BOOTINFO *binfo = 0x00ff0;
+    struct BOOTINFO *binfo = (struct BOOTINFO*) 0x00ff0;
 
     int xsize = binfo->screenX;
     int ysize = binfo->screenY;
     char* vram = binfo->vram;
     
+    init_gdtidt();
     init_screen(vram, xsize, ysize);
 
-    putfont8(vram, xsize, 8, 8, COL8_ffffff, hankaku + 'A'*16);
+    // putfont8(vram, xsize, 8, 8, COL8_ffffff, hankaku + 'A'*16);
+    putfonts8_asci(vram, xsize, 8, 8, COL8_ffffff, "ABC 123");
+    putfonts8_asci(vram, xsize, 31, 31, COL8_000000, "Haribote os.");
+    putfonts8_asci(vram, xsize, 30, 30, COL8_ffffff, "Haribote os.");
+
+    char* s = "";
+    sprintf(s, "cylinders = 0x%05x", binfo->cylinders);
+    putfonts8_asci(vram, xsize, 30, 60, COL8_ffffff, s);
+
+    char* mcursor = "";
+    int mx = 160,
+        my = 100;
+    init_cursor(mcursor, COL8_008484);
+    putblock8_8(vram, xsize, 16, 16, mx, my, mcursor, 16);
 
     for(;;){
         io_hlt();
@@ -151,3 +205,107 @@ void putfont8(char* vram, int xsize, int x, int y, char color, char *font){
     return;
 }
 
+void putfonts8_asci(char* vram, int xsize, int x, int y, char color, unsigned char *s){
+    extern char hankaku[4096];
+
+    for(; *s != 0x00; s++){
+        putfont8(vram, xsize, x, y, color, hankaku + *s * 16);
+        x += 8;
+    }
+    return;
+}
+
+void init_cursor(char *mouse, char backgroundColor){
+    static char cursor[16][16] = {
+        "**************..",
+        "*OOOOOOOOOOO*...",
+        "*OOOOOOOOOO*....",
+        "*OOOOOOOOO*.....",
+        "*OOOOOOOO*......",
+        "*OOOOOOO*.......",
+        "*OOOOOOO*.......",
+        "*OOOOOOOO*......",
+        "*OOOO**OOO*.....",
+        "*OOO*..*OOO*....",
+        "*OO*....*OOO*...",
+        "*O*......*OOO*..",
+        "**........*OOO*.",
+        "*..........*OOO*",
+        "............*OO*",
+        "..............**"
+    };
+    int x,y;
+
+    for(y=0; y < 16; y++){
+        for(x=0; x<16; x++){
+            if(cursor[y][x] == '*'){
+                mouse[y*16 + x] = COL8_000000;
+            }
+            if(cursor[y][x] == 'O'){
+                mouse[y*16 + x] = COL8_ffffff;
+            }
+            if(cursor[y][x] == '.'){
+                mouse[y*16 + x] = backgroundColor;
+            }
+        }
+    }
+
+    return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize){
+    int x,y;
+    for(y=0; y < pysize; y++){
+        for(x=0; x < pxsize; x++){
+            vram[(py0+y)*vxsize + (px0 + x)] = buf[y*bxsize + x];
+        }
+    }
+    return;
+}
+
+void init_gdtidt(void){
+    struct SEGMENT_DESCRIPTOR   *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+    struct GATE_DESCRIPTOR      *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
+
+    // setup Global (segment) Descriptor Table
+    int i;
+    for(i=0; i < 8192; i++){
+        set_segmdesc(gdt + i, 0, 0, 0);
+    }
+    set_segmdesc(gdt+1, 0xffffffff, 0x00000000, 0x4092);
+    set_segmdesc(gdt+2, 0x0007ffff, 0x00280000, 0x409a);
+    load_gdtr(0xffff, 0x00270000);
+
+
+    // setup Interrupt Descriptor Table
+    for(i=0; i < 256; i++){
+        set_gatedesc(idt+i, 0,0,0);
+    }
+    load_idtr(0x7ff, 0x0026f800);
+
+    return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar){
+    if(limit > 0xfffff){
+     ar |= 0x8000;
+     limit /= 0x1000;   
+    }
+
+    sd->limit_low       = limit & 0xffff;
+    sd->base_low        = base & 0xffff;
+    sd->base_mid        = (base >> 16) & 0xff;
+    sd->access_right    = ar & 0xff;
+    sd-> limit_high     = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high       = (base >> 24) & 0xff;
+    return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar){
+    gd->offset_low      = offset & 0xffff;
+    gd->selector        = selector;
+    gd->dw_count        = (ar >> 8) & 0xff;
+    gd->access_right    = ar & 0xff;
+    gd->offset_high     = (offset >> 16) & 0xffff;
+    return;
+}
--- tolset/haribO/naskfunc.nas
+++ tolset/haribO/naskfunc.nas
@@ -8,6 +8,7 @@ GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
 GLOBAL _io_in8, _io_in16, _io_in32
 GLOBAL _io_out8, _io_out16, _io_out32
 GLOBAL _io_load_eflags, _io_store_eflags
+GLOBAL _load_gdtr, _load_idtr
 
 [SECTION .text]
 
@@ -72,4 +73,16 @@ _io_store_eflags:
 	MOV EAX, [ESP+4]
 	PUSH EAX
 	POPFD
+	RET
+
+_load_gdtr:
+	MOV 	AX, [ESP+4]
+	MOV 	[ESP+6], AX
+	LGDT 	[ESP+6]
+	RET
+
+_load_idtr:
+	MOV 	AX, [ESP+4]
+	MOV 	[ESP+6], AX
+	LIDT 	[ESP+6]
 	RET
